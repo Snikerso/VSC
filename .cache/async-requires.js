d2c21f7f34366e96d4711cc59aee98e5d03d22e6
Snikerso <42245963+Snikerso@users.noreply.github.com>
2020-01-13 10:12:18 +0100

    public

---
+++ .cache/async-requires.js
@@ -0,0 +1,20 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---src-templates-aktualnosci-post-js": () => import("../src/templates/aktualnosci-post.js" /* webpackChunkName: "component---src-templates-aktualnosci-post-js" */),
+  "component---src-pages-404-js": () => import("../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-aktualnosci-js": () => import("../src/pages/aktualnosci.js" /* webpackChunkName: "component---src-pages-aktualnosci-js" */),
+  "component---src-pages-badania-js": () => import("../src/pages/badania.js" /* webpackChunkName: "component---src-pages-badania-js" */),
+  "component---src-pages-index-js": () => import("../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-kalendarz-js": () => import("../src/pages/kalendarz.js" /* webpackChunkName: "component---src-pages-kalendarz-js" */),
+  "component---src-pages-kontakt-js": () => import("../src/pages/kontakt.js" /* webpackChunkName: "component---src-pages-kontakt-js" */),
+  "component---src-pages-narzedzia-hardware-js": () => import("../src/pages/narzedzia-hardware.js" /* webpackChunkName: "component---src-pages-narzedzia-hardware-js" */),
+  "component---src-pages-narzedzia-software-js": () => import("../src/pages/narzedzia-software.js" /* webpackChunkName: "component---src-pages-narzedzia-software-js" */),
+  "component---src-pages-ogloszenia-js": () => import("../src/pages/ogloszenia.js" /* webpackChunkName: "component---src-pages-ogloszenia-js" */),
+  "component---src-pages-onas-js": () => import("../src/pages/onas.js" /* webpackChunkName: "component---src-pages-onas-js" */),
+  "component---src-pages-oprogramowanie-js": () => import("../src/pages/oprogramowanie.js" /* webpackChunkName: "component---src-pages-oprogramowanie-js" */),
+  "component---src-pages-osoby-js": () => import("../src/pages/osoby.js" /* webpackChunkName: "component---src-pages-osoby-js" */),
+  "component---src-pages-publikacje-js": () => import("../src/pages/publikacje.js" /* webpackChunkName: "component---src-pages-publikacje-js" */)
+}
+
---
+++ .cache/sync-requires.js
@@ -0,0 +1,23 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---src-templates-aktualnosci-post-js": hot(preferDefault(require("C:\\Users\\Lenovo\\Desktop\\eyetracking_page\\eyetracking\\src\\templates\\aktualnosci-post.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\Lenovo\\Desktop\\eyetracking_page\\eyetracking\\src\\pages\\404.js"))),
+  "component---src-pages-aktualnosci-js": hot(preferDefault(require("C:\\Users\\Lenovo\\Desktop\\eyetracking_page\\eyetracking\\src\\pages\\aktualnosci.js"))),
+  "component---src-pages-badania-js": hot(preferDefault(require("C:\\Users\\Lenovo\\Desktop\\eyetracking_page\\eyetracking\\src\\pages\\badania.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\Lenovo\\Desktop\\eyetracking_page\\eyetracking\\src\\pages\\index.js"))),
+  "component---src-pages-kalendarz-js": hot(preferDefault(require("C:\\Users\\Lenovo\\Desktop\\eyetracking_page\\eyetracking\\src\\pages\\kalendarz.js"))),
+  "component---src-pages-kontakt-js": hot(preferDefault(require("C:\\Users\\Lenovo\\Desktop\\eyetracking_page\\eyetracking\\src\\pages\\kontakt.js"))),
+  "component---src-pages-narzedzia-hardware-js": hot(preferDefault(require("C:\\Users\\Lenovo\\Desktop\\eyetracking_page\\eyetracking\\src\\pages\\narzedzia-hardware.js"))),
+  "component---src-pages-narzedzia-software-js": hot(preferDefault(require("C:\\Users\\Lenovo\\Desktop\\eyetracking_page\\eyetracking\\src\\pages\\narzedzia-software.js"))),
+  "component---src-pages-ogloszenia-js": hot(preferDefault(require("C:\\Users\\Lenovo\\Desktop\\eyetracking_page\\eyetracking\\src\\pages\\ogloszenia.js"))),
+  "component---src-pages-onas-js": hot(preferDefault(require("C:\\Users\\Lenovo\\Desktop\\eyetracking_page\\eyetracking\\src\\pages\\onas.js"))),
+  "component---src-pages-oprogramowanie-js": hot(preferDefault(require("C:\\Users\\Lenovo\\Desktop\\eyetracking_page\\eyetracking\\src\\pages\\oprogramowanie.js"))),
+  "component---src-pages-osoby-js": hot(preferDefault(require("C:\\Users\\Lenovo\\Desktop\\eyetracking_page\\eyetracking\\src\\pages\\osoby.js"))),
+  "component---src-pages-publikacje-js": hot(preferDefault(require("C:\\Users\\Lenovo\\Desktop\\eyetracking_page\\eyetracking\\src\\pages\\publikacje.js")))
+}
+
